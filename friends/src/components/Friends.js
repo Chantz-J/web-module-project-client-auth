570a2418b758092560999d7ba53d09ea8c497ee7
Chantz-J <johnson.chantz@gmail.com>
2021-03-23 21:08:21 -0400

    Token set to local storage. Token not being sent in header of Friends component's axios call.

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -1,11 +1,31 @@
 import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 export default function Friends() {
     const [friends, setFriends] = useState([])
     useEffect(() => {
-
+        axios
+        .get(`http://localhost:5000/api/friends`, 
+        { headers:  
+            { authorization: localStorage.getItem('token') 
+        }})
+        .then(res => {
+            console.log(res)
+        })
+        .catch(err => {
+            console.error(`There was an error retrieving friends: ${err}`)
+        })
     }, [])
     return (
-        <h1>All your friends 🤩 (Should not be able to see this unless you're logged in!)</h1>
+        <>
+        <h1>All your friends 🤩</h1>
+        {/* {
+            friends.map(friend => {
+                return (
+                    <div className="friend">Hello</div>
+                )
+            })
+        } */}
+        </>
     )
 }
